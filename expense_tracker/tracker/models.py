25baf8db4d8264d87c20061ce74c8314ffe56aed
Alfred Wisdom <wisdomalfred964@gmail.com>
2025-10-11 16:17:36 +0100

    update URL routing and add Pipfile and Pipfile.lock for dependency management

--- expense_tracker/tracker/models.py
+++ expense_tracker/tracker/models.py
@@ -11,7 +11,6 @@ class Category(models.Model):
     name = models.CharField(max_length=100)
     type = models.CharField(max_length=10, choices=TYPE_CHOICES)
 
-
     def __str__(self):
         return f"{self.name} ({self.type})"
 
