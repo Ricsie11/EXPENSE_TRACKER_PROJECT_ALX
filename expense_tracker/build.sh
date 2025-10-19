def4d43e1c3f54a6a620cbfa8e5999708f6abd38
Alfred Wisdom <wisdomalfred964@gmail.com>
2025-10-19 01:15:37 +0100

    Add Render deployment config

--- expense_tracker/build.sh
+++ expense_tracker/build.sh
@@ -1,7 +1,12 @@
-set -o errexit
+#!/usr/bin/env bash
+# Exit immediately if a command exits with a non-zero status
+set -o errexit  
 
+# Install dependencies
 pip install -r requirements.txt
 
-python manage.py collectstatic --noinput
+# Collect static files (safe even if you don't have any)
+python manage.py collectstatic --noinput  
 
+# Run database migrations
 python manage.py migrate
